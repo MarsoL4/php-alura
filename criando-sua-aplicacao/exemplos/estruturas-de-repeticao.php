a54985d7678fd6ce6f2a1b93b3f163304446a20d
MarsoL4 <enzogmarsola@gmail.com>
2026-03-25 20:06:01 -0300

    adiciona exemplos de uso de while e do

---
+++ criando-sua-aplicacao/exemplos/estruturas-de-repeticao.php
@@ -0,0 +1,25 @@
+<?php
+// ** Laço For **:
+// Explicação do For em PHP:
+// for (inicializacao; condicao da repeticao; incremento) {
+
+// }
+
+// Exemplo de uso:
+for ($contador = 1; $contador < $argc; $contador += 1) { // O laço For só irá parar quando o contador for maior que a quantidade de parametros recebidos no terminal ao executar o codigo
+    $somaDeNotas += $argv[$contador];
+}
+
+// ** Laço While **:
+// Exemplo de soma de Notas com While
+$contador = 1;
+while ($argv[$contador] != 0) { // Enquanto tiver parametros passados (diferentes de 0) recebidos na linha de comando no terminal ao executar o programa, o while será executado
+    $somaDeNotas += $argv[$contador++];
+}
+
+// ** Laço Do **:
+// Explicação do laço Do
+// Executa alguma ação enquanto a condiçao no while for true
+do {
+    //alguma ação
+} while (true /*condicao*/);
